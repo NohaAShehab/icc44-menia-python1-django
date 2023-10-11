5b592858748cbe90ef5f53465cbd386ffefb235d
Noha Shehab <nshehab@iti.gov.eg>
2023-10-11 16:45:38 +0300

    template inheritence

--- iti/students/views.py
+++ iti/students/views.py
@@ -30,6 +30,25 @@ def students_index(request):
     return HttpResponse(students)
 
 
+# def students_profile(request, id):
+#     # print(type(id))
+#     # id= int(id)
+#     filtered_student = filter(lambda std: std['id'] == id, students)
+#     filtered_student = list(filtered_student)
+#     print(filtered_student)
+#     if filtered_student:
+#         print(filtered_student[0])
+#         return HttpResponse(filtered_student[0])
+#
+#     return HttpResponse("No such student Student ")
+
+
+def students_list(request):
+    # send data to the template
+    return render(request, 'students/index.html', context={"students":students})
+
+
+
 def students_profile(request, id):
     # print(type(id))
     # id= int(id)
@@ -38,13 +57,7 @@ def students_profile(request, id):
     print(filtered_student)
     if filtered_student:
         print(filtered_student[0])
-        return HttpResponse(filtered_student[0])
+        # return HttpResponse(filtered_student[0])
+        return  render(request, 'students/profile.html', context={"student":filtered_student[0]})
 
     return HttpResponse("No such student Student ")
-
-
-def students_list(request):
-    # send data to the template
-    return render(request, 'students/index.html', context={"students":students})
-
-
