830d0972e70bd4ada13f773b8b4f2888ebb0a040
Noha Shehab <nshehab@iti.gov.eg>
2023-10-11 17:30:39 +0300

    creating models and migrations

--- iti/iti/settings.py
+++ iti/iti/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'iti.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangomenia',
+        'USER': 'pymenia',
+        'PASSWORD': 'iti',
+        "HOST": 'localhost',
+        'POST': 5432
     }
 }
 
--- iti/students/models.py
+++ iti/students/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=200,null=True, unique=True)
+    image = models.CharField(max_length=200, null=True)
+    grade=  models.IntegerField(default=100, null=True)
+    gender = models.CharField(
+        choices=[('m', 'Male'), ('f', 'female')]
+    )
+    created_at= models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
